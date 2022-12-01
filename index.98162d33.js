e0c5c19b66e89058ade201c9ca9662ee97d863b7
smerch88 <34419542+smerch88@users.noreply.github.com>
2022-12-01 23:08:34 +0000

    Deploying to gh-pages from @ smerch88/FireBase-test@d54bd984313b182c134c2039036777013f2a693c 🚀

---
+++ index.98162d33.js
@@ -0,0 +1,2 @@
+firebase.initializeApp({apiKey:"",authDomain:"",projectId:"",storageBucket:"",messagingSenderId:"",appId:""});firebase.auth(),firebase.database();
+//# sourceMappingURL=index.98162d33.js.map
---
+++ index.dd3243ec.js
@@ -0,0 +1,2 @@
+!function(){firebase.initializeApp({apiKey:"",authDomain:"",projectId:"",storageBucket:"",messagingSenderId:"",appId:""});firebase.auth(),firebase.database()}();
+//# sourceMappingURL=index.dd3243ec.js.map
